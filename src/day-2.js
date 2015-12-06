20e086bf46a219917c80cb41ede215ca6f94db9d
Yaraslau Kurmyza <me@yaraslav.com>
2015-12-06 14:11:50 +0100

    Day-2 extended with 2nd task

--- src/day-2.js
+++ src/day-2.js
@@ -4,31 +4,45 @@ process.stdin.setEncoding('utf8');
 
 var data = '';
 process.stdin.on('readable', function () {
-    data += process.stdin.read();
+    let chunk = process.stdin.read();
+    if (chunk) {
+        data += chunk;
+    }
 });
 
 process.stdin.on('end', function () {
     var total = 0;
-    data.split("\n").forEach(function (dimensions) {
-        if (dimensions && (dimensions.match(/x/ig) || []).length == 2) {
+    var ribbon = 0;
+
+    var parcels = data.split("\n").map((str) => {
+        // sort by small -> big
+        var dims = (str.split('x') || []);
+        dims.forEach(function (val, key) {
+            dims[key] = parseInt(val);
+        });
+        dims.sort(function (a, b) {
+            return a - b;
+        });
+        return dims;
+    });
+
+    parcels.forEach((dimensions) => {
+        if (dimensions && dimensions.length == 3) {
             total += getAreaForParcel(dimensions);
+            ribbon += getRibbonLengthForParce(dimensions);
         }
     });
 
-    console.log(total);
+    console.log(total, ribbon);
 });
 
-
-function getAreaForParcel(dimensions) {
-    var dims = (dimensions.split('x') || []);
-    dims.forEach(function(val, key) {
-        dims[key] = parseInt(val);
-    });
-    dims.sort(function(a, b) {
-        return a - b;
-    });
-    console.log(dims);
-    return 3 * (dims[0] * dims[1]) +
+function getAreaForParcel(dims) {
+    return 3 * (dims[0] * dims[1]) +  // 2 + 1 extra
         2 * (dims[1] * dims[2]) +
         2 * (dims[0] * dims[2]);
+}
+
+function getRibbonLengthForParce(dims) {
+    return 2 * (dims[0] + dims[1]) + // smallest perimiter
+        dims[0] * dims[1] * dims[2]; // volume
 }
