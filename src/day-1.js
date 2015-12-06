e231a78972937bb39dab0008344d007e899b6e58
Yaraslau Kurmyza <me@yaraslav.com>
2015-12-06 14:19:53 +0100

    Day-1 extended with 2nd task

--- src/day-1.js
+++ src/day-1.js
@@ -1,13 +1,33 @@
+'use strict';
 
 process.stdin.setEncoding('utf8');
 
 var data = '';
-process.stdin.on('readable', function() {
-  data += process.stdin.read();
+process.stdin.on('readable', function () {
+    let chunk = process.stdin.read();
+    if (chunk) {
+        data += chunk;
+    }
 });
 
-process.stdin.on('end', function() {
-  console.log(
-      (data.match(/\(/ig) || []).length - (data.match(/\)/ig) || []).length
-  );
+process.stdin.on('end', function () {
+    // 1st part
+    console.log(
+        (data.match(/\(/ig) || []).length - (data.match(/\)/ig) || []).length
+    );
+
+    // 2nd part
+    let curPos = 1;
+    for (let i = 0; i < data.length; i++) {
+        if (data.charAt(i) === '(') {
+            curPos += 1;
+        } else if (data.charAt(i) === ')') {
+            curPos -= 1;
+        }
+
+        if (curPos === 0) {
+            console.log(i+1);
+            break;
+        }
+    }
 });
